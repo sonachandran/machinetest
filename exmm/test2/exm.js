37e6befc061a027b7e5a77a0a15afde4c46a57d0
sonachandran <144424737+sonachandran@users.noreply.github.com>
2024-05-23 13:46:44 +0530

    Add files via upload

---
+++ exmm/test2/exm.js
@@ -0,0 +1,15 @@
+let row=6;
+for(let i=0;i<row;i++)
+ {
+    for(let j=0;j<i;j++)   
+        {  
+          
+            document.write("*") 
+          
+         
+         
+    }
+       document.write("<br>") 
+
+}
+
